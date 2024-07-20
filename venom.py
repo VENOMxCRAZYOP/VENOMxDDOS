59b4ab9acaebc0b4c843f7418b3bf57304b91586
NEW-COMMONER <ikitsfake7@gmail.com>
2024-07-20 18:48:26 +0530

    Update venom.py

--- venom.py
+++ venom.py
@@ -4,8 +4,8 @@ import logging
 from aiogram import Bot
 import asyncio
 
-API_TOKEN = 'TOKEN'
-ADMIN_ID = 'YOUR ID'
+API_TOKEN = '7253917965:AAH6mG6AHe8PN0RM45CjA-KMS46mFt9FtXM'
+ADMIN_ID = '1244392555'
 MAX_RESTARTS = 5
 RESTART_PERIOD = 60  # Seconds
 
